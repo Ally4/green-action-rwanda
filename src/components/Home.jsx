cf3ac0aee794be964c131a7df023180deae3bcc2
ally4 <el.ally741@gmail.com>
2023-07-01 13:17:56 +0200

    the beginning

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import "./HomeStyle.css";
+
+function Home() {
+  return (
+    <>
+      <div className="home">
+        <h1>Brigdeton: get your good</h1>
+      </div>
+    </>
+  );
+}
+
+export default Home;
