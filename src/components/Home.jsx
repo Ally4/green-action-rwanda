ddf550629306ad406e1889711b45d2d84e495447
ally4 <el.ally741@gmail.com>
2023-07-03 19:40:39 +0200

    the styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,57 @@ import "./HomeStyle.css";
 function Home() {
   return (
     <>
+    <div>
+      <div className="home-v">
+        <h1 className="vision-t">Vision</h1>
+        <div className="vision">
+        <div className="vision-p">WE ARE CREATING AN ONLINE TRACKING AND TRACEABILITY SYSTEM TO HELP AVOCADO FARMERS ACCESS LOCAL AND EXPORT MARKET IN ORDER TO EARN MORE INCOME THAN THEY USUALLY DO FROM ALL AVOCADO VARIETIES.</div>
+        <div className="vision-p1">PLACING SOLAR POWER CONTAINER COLD ROOMS IN STRATEGIC PLACES THAT ARE CLOSE TO THE FARMERS TO STORE, TO SEPARATE (THOSE MEANT FOR EXPORT, FOR SUPPLY AS RAW MATERIALS FOR AVOCADO ADD VALUE FACTORIES OR FOR LOCAL MARKET CONSUMPTION) AND COLLECTING CENTERS FOR THEIR HARVEST.</div>
+        <div className="vision-p2">MARKET FOR ALL AVOCADO VARIETIES HARVEST, ACCESS TO CARBON CREDIT FUNDS AND WELL STRUCTURED SUPPLY CHAIN HENCE MOTIVATE FARMERS TO PLANT MORE AVOCADO TREES </div>
+      </div>
+      </div>
       <div className="home">
-        <h1>Brigdeton: get your good</h1>
+        <h1 className="problem-t">Problems</h1>
+      <div className="problem">
+        <div className="problem-p">FARMERS GAIN LESS INCOME FROM THEIR AVOCADO FARMING</div>
+        <div className="problem-p1">PAPART FROM HASS AND FUERTE WHICH DON’T EVEN MAKE IT FOR EXPORT 100%, THERE IS A SMALL FRAGMENTED MARKET AND NO ADD VALUE OF OTHER AVOCADO VARIETES</div>
+        <div className="problem-p2">DESPITE PLANTING AND EXPORTING AVOCADO FRUITS, THERE IS NO MEAN OF TRACKING AND TRACING TREES AND WHERE THE HARVEST ORIGINATED.</div>
+        <div className="problem-p3">POOR LOGISTICS IN THE AVOCADO SUPPLY CHAIN</div>
+        <div className="problem-p4">FARMERS DON’T GET ACCESS TO CARBON CREDITS FUNDS DUE TO THEIR IGNORANCE ON THE MATTER OR LACK MEANS TO ACCESS THEM.</div>
+      </div>
+      </div>
+      <div className="home">
+        <h1 className="solution-t">Solutions</h1>
+      <div className="solution">
+        <h2>BY USING GPS & BLOCKCHAIN SYSTEMS IN OUR PLATFORM AND WORKING WITH OAF OUTGROWERS AND SITE CORDINATORS WE WILL;</h2>
+        <div className="solution-p">TRACK AVOCADO TREES BY INCLUDING WHERE THE TREE IS LOCATED, TYPE OF THE TREE AND THE DAY IT WAS PLANTED HENCE HELP FARMERS GET ACCESS TO CARBON CREDITS AND FOLLOW UP ON THE SURVIVAL RATE OF THE SEEDLINGS PROVIDED BY OAF.</div>
+        <div className="solution-p1">EXPORT TRACEABLE HARVEST BY PUTTING A QR CODE STICKERS ON THE AVOCADOS</div>
+        <div className="solution-p2">COLLECT THE HARVEST AND STORE THEM INTO SOLAR POWER GENERATED COLD ROOMS WHICH WILL BE LOCATED NEAR THE FARMERS</div>
+        <div className="solution-p3">PROVIDE A MARKET FOR ALL AVOCADO VARIETES HARVEST</div>
+      </div>
+      </div>
+      <div className="home">
+        <h1 className="business-t">Businesss</h1>
+      <div className="business">
+        <div className="business-p">BUYING & EXPORTING TRACEABLE AVOCADOS FROM FARMERS BY USING QR CODES STICKERS ON THE FRUITS</div>
+        <div className="business-p1">GAINING COMMISSION FROM THE CARBON CREDITS</div>
+        <div className="business-p2">SUPPLY RAW MATERIALS TO FACTORIES THAT ADD VALUE TO AVOCADO HARVEST</div>
+      </div>
+      </div>
+      <div className="home">
+        <h1 className="traction-t">Traction</h1>
+      <div className="traction">
+        <div className="traction-p">WE HAVE AN MOU WITH KIIWP PROJECT THAT HAVE PLANTED MORE THAN 60,000 AVOCADO TREES.</div>
+        <div className="traction-p1">AN MOU WITH “I CHILL” A COMPANY THAT MANUFACTURES SOLAR POWER COLD ROOM CONTAINERS IN RWANDA TO SUPPLY US</div>
+      </div>
+      </div>
+      <div className="home">
+        <h1 className="team-t">Team</h1>
+      <div className="team">
+        <div className="team-p">BINEZA RICHARD, A JASIRI FELLOW AND CEO WITH MORE THAN 5 YEARS IN BUSINESS MANAGEMENT</div>
+        <div className="team-p1">NENGO ALLY, A JASIRI FELLOW AND CTO WITH MORE THAN 4 YEARS IN SOFTWARE ENGINEERING.</div>
+      </div>
+      </div>
       </div>
     </>
   );
